e3ae63f5526087e394ea18b416f1fa4b238f012f
Mohamed Hadji <59908103+mohaahadji@users.noreply.github.com>
2020-10-18 09:52:48 +0700

    Add files via upload

---
+++ main.js/main.js
@@ -0,0 +1,55 @@
+const api = {
+    key: "facb953c06a091312729704bc26f48e6", 
+    baserul: "http://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+
+function setQuery(evt){
+    if (evt.keyCode == 13){
+        getResults(searchbox.value);
+        console.log(searchbox.value);
+    }
+}
+
+function getResults(query){
+    fetch(`${api.baserul}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather=>{
+        return weather.json();
+    }) .then(displayResults);
+}
+
+function displayResults(weather) {
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}`
+
+    let el_weather = document.querySelector('.current .weather');
+    el_weather.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.weather .hi-low');
+    hilow.innerText = `${weather.main.temp_min}°c / ${weather.main.temp_max}°c`;
+
+}
+
+function dateBuilder(d){
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "tuesday","wednesday","Thursday", "Friday", "saturday" ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+
+
+}
